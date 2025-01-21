c157e2e8f752f0de65e21bfcd29705ed938fb5e7
geen <gqkuah2505@gmail.com>
2025-01-21 16:52:41 +0800

    modified:   index.js 	deleted:    planning.md 	renamed:    public/styles/styles.css -> public/styles/header.css 	new file:   public/styles/index.css 	modified:   views/index.ejs 	modified:   views/partials/header.ejs 	new file:   views/submit.ejs

--- index.js
+++ index.js
@@ -7,8 +7,24 @@ const port = 3000;
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: true }));
 
+let posts = [];
+
 app.get("/", (req, res) => {
-  res.render("index.ejs");
+  res.render("index.ejs", { posts });
+});
+
+app.post("/", (req, res) => {
+  res.render("submit.ejs");
+});
+
+app.get("/submit", (req, res) => {
+  res.render("submit.ejs");
+});
+
+app.post("/submit", (req, res) => {
+  const { Title, Subheader, paragraph } = req.body;
+  posts.push({ title: Title, subheader: Subheader, paragraph });
+  res.redirect("/");
 });
 
 app.listen(port, () => {
