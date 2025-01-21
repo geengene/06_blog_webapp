8ed339a747339be27c1c9f6d83b608af0a5dcc70
geen <gqkuah2505@gmail.com>
2025-01-21 23:05:00 +0800

    modified:   index.js 	modified:   views/edit.ejs 	modified:   views/index.ejs 	modified:   views/submit.ejs

--- index.js
+++ index.js
@@ -31,7 +31,21 @@ app.get("/edit/:id", (req, res) => {
   console.log(req.params);
   const postId = req.params.id;
   const post = posts[postId];
-  res.render("edit.ejs", { post });
+  res.render("edit.ejs", { post, postId });
+});
+
+app.post("/edit/:id", (req, res) => {
+  const postId = req.params.id;
+  const { title, subheader, paragraph } = req.body;
+  posts[postId] = { title, subheader, paragraph };
+  res.redirect("/");
+});
+
+app.get("/delete/:id", (req, res) => {
+  console.log(req.params);
+  const postId = req.params.id;
+  posts.splice(postId, 1);
+  res.redirect("/");
 });
 
 app.listen(port, () => {
